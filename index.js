fba2d31117e9a4f35359c7edcfb39ca258987884
Mohamad Zabad <zabadmohamad@yahoo.com>
2019-01-12 00:45:17 +0000

    Test commit to test gitignore + connection added

---
+++ index.js
@@ -0,0 +1,28 @@
+// Importing the sensitive data that shouldn't reach github
+var keys = require('./keys.json')
+
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
+
+// Log our bot in using the token
+client.login(keys.token);
